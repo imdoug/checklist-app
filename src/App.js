dd02aaafeccb32a5c29e8e2a1291e817484d8af2
Doug Moreira <dougmoreira@Dougs-MBP-170.lan>
2022-03-28 20:34:32 -0400

    app created

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'>If you dont love yourself, how in the hell you're gonna love somebody else?</div>
+  )
+}
+
+export default App
